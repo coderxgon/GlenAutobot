215f1a5d053de182cf0fd87fd991c83d4017280d
coderxgon <glenalberth2011@gmail.com>
2025-09-03 03:10:56 +0000

    4

--- auto.js
+++ auto.js
@@ -209,8 +209,8 @@ app.post('/login', async (req, res) => {
     });
   }
 });
-app.listen(3000, () => {
-  console.log(`Server is running at http://localhost:5000`);
+app.listen(5000, '0.0.0.0', () => {
+  console.log(`Server is running at http://0.0.0.0:5000`);
 });
 process.on('unhandledRejection', (reason) => {
   console.error('Unhandled Promise Rejection:', reason);
